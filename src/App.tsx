43edafdd906d6b3cb5936400de63cd12916f747f
Wanderson Paes <ecompwandersonpaes@gmail.com>
2026-05-17 23:19:39 -0300

    feat: starting pride bingo project

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>Pride Bingo</h1>
+        </div>
+      </section>
+
+      <div className="ticks"></div>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
